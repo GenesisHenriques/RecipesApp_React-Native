1de728faa2a205909537ec2852baa496c6f5daba
Genesis-Henriques <GenesisHenriques@gmail.com>
2022-05-05 00:36:09 -0300

    :rocket: Partially created login screen

---
+++ RecipesApp/App.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+// import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+
+import Login from './src/Pages/Login';
+// import Home from './src/Pages/Home';
+
+
+export default function App() {
+  return (
+    <Login />
+  );
+}
